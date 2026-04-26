14b4b5f101e5a5e4308e294bbe01f577f1b5e7df
= <ahmadwahebarifi@gmail.com>
2026-04-26 14:49:24 +0430

    Migrationg

--- database/migrations/2026_04_22_113215_create_accounts_table.php
+++ database/migrations/2026_04_22_113215_create_accounts_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
             $table->string('name'); // Cash, Bank, etc.
             $table->decimal('balance', 15, 2)->default(0);
             $table->timestamps();
